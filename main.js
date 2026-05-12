5988d9a51244dbd675f2ec126a5b1f3814cf800e
Febriany Renata <96133771+FebrianyRenata02@users.noreply.github.com>
2026-05-12 15:15:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -245,7 +245,7 @@ about.className = "about";
 about.id = "about";
 about.innerHTML = `
   <div class="about-left">
-    <img src="reta.png" alt="Founder">
+    <img src="img/RenataFounder.png" alt="Founder">
   </div>
   <div class="about-right">
     <h2>San Digital Agency</h2>
